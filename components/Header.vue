0a6eeb27b851983abfb1433f62a394cb30d6046f
Hoàng Anh Lâm <comficker@gmail.com>
2023-06-22 08:26:21 +0700

    Init

--- components/Header.vue
+++ components/Header.vue
@@ -6,8 +6,8 @@ const is_home = computed(() => route.name === "index")
 <template>
   <div class="max-w-xl mx-auto w-full px-4 md:px-0">
     <h1>
-      <nuxt-link class="flex items-center gap-1" to="/">
-        <div class="i-con-palette fill-red-400" :class="{'h-10': is_home, 'h-5': !is_home}"/>
+      <nuxt-link class="flex gap-1" to="/">
+        <div class="i-con-pad fill-red-400" :class="{'h-10 w-10': is_home, 'h-5 w-5': !is_home}"/>
         <img class="w-auto" src="/logo.png" alt="" :class="{'h-12': is_home, 'h-6': !is_home}">
       </nuxt-link>
     </h1>
