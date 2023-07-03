90e75f93457b60e5b4dde06ed8f9da667f643f62
Hoàng Anh Lâm <comficker@gmail.com>
2023-07-03 23:35:58 +0700

    Init

--- components/Header.vue
+++ components/Header.vue
@@ -5,12 +5,17 @@ const is_home = computed(() => route.name === "index")
 
 <template>
   <div class="max-w-xl mx-auto w-full px-4 md:px-0">
-    <h1>
-      <nuxt-link class="flex gap-1" to="/">
-        <div class="i-con-pad fill-red-400" :class="{'h-10 w-10': is_home, 'h-5 w-5': !is_home}"/>
-        <img class="w-auto" src="/logo.png" alt="" :class="{'h-12': is_home, 'h-6': !is_home}">
+    <div class="flex justify-between">
+      <h1>
+        <nuxt-link class="flex gap-1" to="/">
+          <div class="i-con-pad fill-red-400" :class="{'h-10 w-10': is_home, 'h-8 w-8': !is_home}"/>
+          <img class="w-auto" src="/logo.png" alt="" :class="{'h-12': is_home, 'h-8': !is_home}">
+        </nuxt-link>
+      </h1>
+      <nuxt-link class="p-2.5 cursor-pointer" to="/editor">
+        <div class="w-4 h-4 i-con-design"></div>
       </nuxt-link>
-    </h1>
+    </div>
     <div v-if="is_home" class="text-lg">Online pixel editor and coloring by number</div>
   </div>
 </template>
