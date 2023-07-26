c0425190491d8edb39a341ec46a367d89cde3b18
Hoàng Anh Lâm <comficker@gmail.com>
2023-07-26 09:56:01 +0700

    Init

--- components/MainNavigator.vue
+++ components/MainNavigator.vue
@@ -14,18 +14,18 @@ watch(() => route.fullPath, () => {
     <div class="w-6 h-6" :class="{'i-con-up': force, 'i-con-down': !force}"/>
   </div>
   <div
-    class="hidden md:flex flex-col md:flex-row absolute md:relative bg-white z-70 right-0 left-0 top-full border-y md:border-none divide-y md:divide-none p-0.5"
+    class="hidden md:flex flex-col md:flex-row absolute md:relative bg-white z-70 right-0 left-0 top-full md:top-0 border-y md:border-none divide-y md:divide-none"
     :class="{'flex': force}"
   >
-    <nuxt-link class="flex gap-2 items-center p-3 hover:text-blue-500" to="/editor">
+    <nuxt-link class="flex gap-2 items-center p-4 md:p-2 hover:text-blue-500" to="/editor">
       <div class="w-4 h-4 i-con-design"></div>
       <span>Pixel editor</span>
     </nuxt-link>
-    <nuxt-link class="flex gap-2 items-center p-3 hover:text-blue-500" to="/pages">
+    <nuxt-link class="flex gap-2 items-center p-4 md:p-2 hover:text-blue-500" to="/pages">
       <div class="w-4 h-4 i-con-color"></div>
       <span>Coloring Pages</span>
     </nuxt-link>
-    <nuxt-link class="flex gap-2 items-center p-3 hover:text-blue-500" to="/arts">
+    <nuxt-link class="flex gap-2 items-center p-4 md:p-2 hover:text-blue-500" to="/arts">
       <div class="w-4 h-4 i-con-picture"></div>
       <span>Pixel Arts</span>
     </nuxt-link>
