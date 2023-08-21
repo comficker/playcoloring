8e413b2da76ba1b9267de1338b515dab8c4080df
Hoàng Anh Lâm <comficker@gmail.com>
2023-08-21 15:50:17 +0700

    Init

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
-export const appName = 'Vitesse for Nuxt 3'
-export const appDescription = 'Vitesse for Nuxt 3'
+export const appName = 'PlayColoring'
+export const appDescription = 'Color Game'
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,7 +51,7 @@ export default defineNuxtConfig({
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { name: 'description', content: appDescription },
-        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+        // { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
       ],
       script: [
         {
