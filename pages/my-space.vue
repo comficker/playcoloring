e0d4efbd9a880d8760f2524f028054aab7d0b2a0
Hoàng Anh Lâm <comficker@gmail.com>
2023-08-30 22:58:06 +0700

    Init

--- pages/my-space.vue
+++ pages/my-space.vue
@@ -5,7 +5,7 @@ import ColoringCard from "~/components/ColoringCard.vue";
 import {useUserStore} from "~/stores/user";
 import {useSeoMeta} from "#app";
 import Breadcrumb from "~/components/Breadcrumb.vue";
-
+const {$touch} = useNuxtApp()
 const userStore = useUserStore()
 userStore.setBC([
   {
@@ -17,6 +17,7 @@ userStore.setBC([
 const page = ref(1)
 
 const response = ref<ResponseSharedPage | null >(null)
+
 useSeoMeta({
   title: "My Space",
   description: 'Your area!',
@@ -29,7 +30,11 @@ useHead({
 })
 
 onMounted(() => {
-  useAuthFetch<ResponseSharedPage>(`/coloring/shared-pages/`, {
+  fetch()
+})
+
+const fetch = function () {
+  $touch(`/coloring/shared-pages/`, {
     params: {
       page: page.value,
       page_size: 32,
@@ -37,16 +42,45 @@ onMounted(() => {
       full_schema: true
     }
   }).then(r => {
-    response.value = r.data.value
+    console.log(r);
+    response.value = r
   })
-})
+}
+
+function handlePaging(p: number) {
+  page.value = p
+  fetch()
+}
+
+watch(() => userStore.logged, () => {
+  page.value = 1
+  fetch()
+}, {deep: true})
 </script>
 
 <template>
   <breadcrumb/>
-  <div class="max-w-xl mx-auto space-y-4">
-    <div v-if="response" class="grid grid-cols-2 md:grid-cols-3 gap-3">
+  <div v-if="response" class="max-w-xl mx-auto space-y-4">
+    <div class="grid grid-cols-2 md:grid-cols-3 gap-3">
       <coloring-card v-for="item in response.results" :value="item"/>
+    </div>
+    <div class="flex font-semibold">
+      <div v-if="response.links.previous" class="btn hover:shadow" @click="handlePaging(page--)">
+        <div class="i-con-left w-6 h-6"/>
+        <span>Previous</span>
+      </div>
+      <div class="flex-1 flex justify-center gap-3">
+        <div class="btn hover:shadow">
+          <span>Current:</span>
+          <span>{{ page }}</span>
+          <span>/</span>
+          <span>{{ response.num_pages }}</span>
+        </div>
+      </div>
+      <div v-if="response.links.next" class="btn hover:shadow" @click="handlePaging(page++)">
+        <span>Next</span>
+        <div class="i-con-right w-6 h-6"/>
+      </div>
     </div>
   </div>
 </template>
