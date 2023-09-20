26b3e8f838276459b831a76c0cf48f30651b1c96
Hoàng Anh Lâm <comficker@gmail.com>
2023-09-21 00:06:37 +0700

    fix: mobile ui

--- server/routes/sitemap-tag.xml.ts
+++ server/routes/sitemap-tag.xml.ts
@@ -4,18 +4,12 @@ import {ofetch} from "ofetch";
 
 export default defineEventHandler(async (event) => {
   let out = '<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="/sitemap-template.xsl"?>'
-  const res: ResponseSharedPage = await ofetch(`${process.env.API}/coloring/shared-pages/`, {
-    query: {
-      page_size: 1000,
-      status: 'public',
-      is_template: true
-    }
-  })
+  const res: ResponseSharedPage = await ofetch(`${process.env.API}/coloring/tags/`)
   const now = new Date()
   out = out + '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
   res.results.forEach(item => {
     out = out + '<url>' +
-      `<loc>https://playcoloring.com/page/${item.id_string}</loc>` +
+      `<loc>https://playcoloring.com/pages/${item.id_string}</loc>` +
       `<lastmod>${now}</lastmod>` +
       '<changefreq>daily</changefreq>' +
       '<priority>0.8</priority>' +
